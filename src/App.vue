92b5c347609b3fe0c1da181aade4bc7af6b3baa7
Moritz Gobbert <gobbert@uni-trier.de>
2022-12-05 12:41:21 +0100

    background image

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Background from './components/Background.vue'
 </script>
 
 <template>
+  <Background />
   <div>
     <HelloWorld />
   </div>
---
+++ src/components/Background.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+const images = ["/images/bg-kairo-1.png",
+    "/images/bg-skyrim-1.png",
+    "/images/bg-wow-1.jpg"];
+
+let current = ref(0);
+
+async function changeBackground() {
+    if (this.current < this.images.length - 1) {
+        this.current = this.current + 1;
+    } else {
+        this.current = 0;
+    }
+
+}
+
+</script>
+
+<template>
+    <Transition name="fade">
+        <img class="bgimg" @click="changeBackground()" :src="images[current]" :key="current" />
+    </Transition>
+</template>
+
+<style scoped>
+.bgimg {
+    z-index: -1;
+    position: absolute;
+    top: 0px;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-size: auto;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.75s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
